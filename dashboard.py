c73ec20b9fba9051c197e836dc51c0e1d3e20a75
Achmad Irfan Afandi <achmad.irfan1@yahoo.com>
2023-05-29 18:28:31 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -13,7 +13,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 from dash.dependencies import Output, Input
-
+import dash_bootstrap_components as dbc
 
 # In[2]:
 
